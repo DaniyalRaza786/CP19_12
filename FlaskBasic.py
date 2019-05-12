afba5a6d022302ec3be09aa5c40152dc943738f5
TahaYamin <49732617+TahaYamin@users.noreply.github.com>
2019-05-12 23:46:50 +0500

    added debug option

--- FlaskBasic.py
+++ FlaskBasic.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def World():
+    return "Hello World!"
+
+@app.route("/taha")
 def Taha():
     return "Hello Taha!"
 @app.route("/daniyal")
