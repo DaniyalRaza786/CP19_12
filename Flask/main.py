716aa59e40e0035371e3b58366f62d54e4517a67
daniyalraza786 <sp19bscs0141@maju.edu.pk>
2019-05-13 06:41:21 +0500

    insert data in database

--- Flask/main.py
+++ Flask/main.py
@@ -1,10 +1,33 @@
-import flask
-from flask import Flask , render_template
+import pyrebase
+config={
+"apiKey": "AIzaSyDYt-fmafI1kkMZSIphL829C6QgdlE1Tro",
+    "authDomain": "cp19-12.firebaseapp.com",
+    "databaseURL": "https://cp19-12.firebaseio.com",
+   " projectId": "cp19-12",
+    "storageBucket": "cp19-12.appspot.com",
+    "messagingSenderId": "681358965828",
+    "appId": "1:681358965828:web:3e31fb7429aed930"
+
+}
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+
+
+#from flask import Flask , render_template,*
+from flask import *
 app = Flask(__name__)
-
-
-@app.route("/")
+@app.route('/', methods=['GET','POST'])
 def hello():
+    if request.method=='POST':
+        name=request.form['feed']
+        db.child("new_post").push(name)
+        new_post=db.child("new_post").get()
+        new=new_post.val()
+        return render_template('Home.html',post=new.values())
+    new_post=db.child("new_post").get()
+    new=new_post.val()
     return render_template('Home.html')
 @app.route("/Taha")
 def Taha():
