db10864fb76d61d8f31814d2e20a79e40ac7f39b
TahaYamin <49732617+TahaYamin@users.noreply.github.com>
2019-05-20 12:32:38 +0500

    Updated File

--- Flask/main.py
+++ Flask/main.py
@@ -14,24 +14,46 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 storage=firebase.storage()
 
-from flask import Flask , render_template,*
+#from flask import Flask , render_template,*
 from flask import *
+from datetime import date
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def hello():
     if request.method=='POST':
         name=request.form['feed']
         images=request.files['picture']
+        
+        
         if name != "":
-             db.child("new_post").push(name)
+             db.child("new_post").push({"post":name})
              new_post=db.child("new_post").get()
              new=new_post.val()
              return render_template('Home.html',post=new.values())
-        
+        if images != "":
+             storage.child("image/new2.jpg").put(images)
+             link=storage.child("image/new2.jpg").get_url(None)
+             return render_template('Home.html',l=link)
     new_post=db.child("new_post").get()
     new=new_post.val()
-    link=storage.child("image/new1.jpg").get_url(None)
+    link=storage.child("image/new2.jpg").get_url(None)
     return render_template('Home.html',post=new.values(),l=link)
+@app.route('/comment', methods=['GET','POST'])
+def comments():
+    if request.method=='POST':
+        comment=request.form['comment']
+        if comment != "":
+            today = date.today()
+            db.child("comment").push({"new_comment":comment,"date":today})
+            new_comment=db.child("comment").get()
+            new_comment_post=new_comment.val()
+            return render_template('Home.html',new_comment_post=new_comment_post.values())
+    new_post=db.child("new_post").get()
+    new=new_post.val()
+    link=storage.child("image/new2.jpg").get_url(None)
+    new_comment=db.child("comment").get()
+    new_comment_post=new_comment.val()
+    return render_template('Home.html',post=new.values(),l=link,new_comment_post=new_comment_post.values())  
     
 @app.route("/Taha")
 def Taha():
